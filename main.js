95992663b6b74230a4eaf7e248e36ebfb5f93d9d
JakelinMishelQuinoMartinez <quinojakelin@gmail.com>
2026-05-14 09:57:03 -0600

    feat: :zap: Se le da funcionalidad al apartado para editar el perfil y se hacen modificaciones en el inicio de sesión
    Se agregan funciones y diseños para permitir que se muestre y oculte el apartado para editar el perfil, para verificar y guardar los datos en el local storage y se agrega opción para confirmar contraseña si se desea editarla

--- main.js
+++ main.js
@@ -5,17 +5,26 @@ const iniciarSecion = document.getElementById("log_in")
 const formulario = document.getElementById("loginForm")
 const header = document.querySelector(".header");
 const mainContent = document.querySelector(".main");
+const userTextHeader = document.querySelector("#perfil .text_hide");
 
 //Credenciales
+let nombreDefault = "Admin"
 const correo = "admin@campusparking.com"
-const contraseña = "Admin123"
+let contraseñaDefault = "Admin123"
+
+//Credenciales del LocalStorage
+let nombreActual = JSON.parse(localStorage.getItem("nuevoNombre")) || nombreDefault;
+let contraseñaActual = JSON.parse(localStorage.getItem("newPassword")) || contraseñaDefault;
+
+//Colocar el nombre del Usuario en el header
+userTextHeader.textContent = nombreActual;
 
 //Verificar Credenciales
 formulario.addEventListener("submit", (e) =>{
     e.preventDefault();
     const pasword = document.getElementById("password").value;
-    const email = document.getElementById("email").value;
-    if (contraseña == pasword && correo == email){
+    const email = document.getElementById("email").value.trim();
+    if (contraseñaActual == pasword && correo == email){
         iniciarSecion.style.display = "none";
         formulario.reset();
     } else {
@@ -34,3 +43,52 @@ cerrarSesion.addEventListener("click", (e) => {
         iniciarSecion.style.display = "flex";
       }
 })
+
+//====================EDITAR PERFIL===================
+
+// Referencias
+const perfilBtn = document.getElementById("perfil");
+const editarPerfil = document.getElementById("edit_profile");
+const btnCancelar = document.getElementById("btn-cancel");
+const profileForm = document.getElementById("profileForm");
+
+// Abrir la sección para editar perfil
+perfilBtn.addEventListener("click", () => {
+    editarPerfil.style.display = "flex";
+    document.getElementById("edit_username").value = nombreActual;
+    document.getElementById("edit_password").value = ""; 
+    document.getElementById("confirm_password").value = "";
+});
+
+//Cerrar la sección de perfil (Botón de Cancelar)
+btnCancelar.addEventListener("click", () => {
+    editarPerfil.style.display = "none";
+    profileForm.reset();
+});
+
+//Validar y guardar cambios (Botón Submit)
+profileForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const nuevoNombre = document.getElementById("edit_username").value;
+    const nuevaContrasena = document.getElementById("edit_password").value.trim();
+    const confirmarPasword = document.getElementById("confirm_password").value.trim();
+    if (nuevaContrasena !== "" || confirmarPasword !== nuevaContrasena) {
+        if (nuevaContrasena !== confirmarPasword) {
+            alert("Las contraseñas no coinciden. Por favor, verifica.");
+            return;
+        }
+    }
+    const decision = confirm("¿Está seguro de que desea guardar estos cambios?");
+    if (decision) {
+        localStorage.setItem("nuevoNombre", JSON.stringify(nuevoNombre));
+        nombreActual = nuevoNombre;
+        if (nuevaContrasena !== "") {
+            localStorage.setItem("newPassword", JSON.stringify(nuevaContrasena));
+            contraseñaActual = nuevaContrasena;
+        }
+        userTextHeader.textContent = nuevoNombre;
+        alert("Perfil actualizado con éxito.");
+        editarPerfil.style.display = "none";
+        profileForm.reset();
+    }
+});
